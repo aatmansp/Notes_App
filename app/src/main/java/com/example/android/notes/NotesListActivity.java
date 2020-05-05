a1756ebda3f4ea4f6eb1467693e0da764cee5544
Aatman Pradhan <aatmansp@rediffmail.com>
2020-05-05 23:35:39 +0530

    Added swipe right to delete feature.

--- app/src/main/java/com/example/android/notes/NotesListActivity.java
+++ app/src/main/java/com/example/android/notes/NotesListActivity.java
@@ -1,22 +1,25 @@
 package com.example.android.notes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.LinearLayout;
+import android.view.View;
 
 import com.example.android.notes.adapters.NotesRecyclerAdapter;
 import com.example.android.notes.models.Note;
 import com.example.android.notes.util.VerticalSpacingItemDecorator;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
-public class NotesListActivity extends AppCompatActivity implements NotesRecyclerAdapter.OnNoteListener {
+public class NotesListActivity extends AppCompatActivity implements NotesRecyclerAdapter.OnNoteListener, View.OnClickListener {
 
     private static final String TAG = "NotesListActivity";
 
@@ -29,9 +32,11 @@ public class NotesListActivity extends AppCompatActivity implements NotesRecycle
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notes);
+        setContentView(R.layout.activity_notes_list);
 
         mRecyclerView = findViewById(R.id.recyclerView);
+
+        findViewById(R.id.fab).setOnClickListener(this);
 
         initRecyclerView();
         insertFakeNoteS();
@@ -58,6 +63,7 @@ public class NotesListActivity extends AppCompatActivity implements NotesRecycle
         mRecyclerView.setLayoutManager(linearLayoutManager);
         VerticalSpacingItemDecorator itemDecorator = new VerticalSpacingItemDecorator(10);
         mRecyclerView.addItemDecoration(itemDecorator);
+        new ItemTouchHelper(itemTouchHelperCallback).attachToRecyclerView(mRecyclerView);
         mNotesRecyclerAdapter = new NotesRecyclerAdapter(mNotes , this);
         mRecyclerView.setAdapter(mNotesRecyclerAdapter);
 
@@ -71,4 +77,27 @@ public class NotesListActivity extends AppCompatActivity implements NotesRecycle
         intent.putExtra("selected_note" , mNotes.get(position));
         startActivity(intent);
     }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this , NoteActivity.class);
+        startActivity(intent);
+    }
+
+    private void deleteNote(Note note){
+        mNotes.remove(note);
+        mNotesRecyclerAdapter.notifyDataSetChanged();
+    }
+
+    private ItemTouchHelper.SimpleCallback itemTouchHelperCallback = new ItemTouchHelper.SimpleCallback(0,ItemTouchHelper.RIGHT) {
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+            deleteNote(mNotes.get(viewHolder.getAdapterPosition()));
+        }
+    };
 }
